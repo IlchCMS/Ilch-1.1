be53909d28db7af67eee0064c35657b6567ebbc0
m0nu <m0nu@5b3287ef-0713-0410-bd1a-a55732392574>
2006-12-30 14:10:01 +0000

    spam teil loescht die ergs nicht nach dem ersten aufruf so koennen grafiken vom browser oefter aufgerufen werden

--- include/images/spam/z.php
+++ include/images/spam/z.php
@@ -9,6 +9,6 @@ $w = intval(preg_replace("/[^0-2]/", "",$_GET['w']));
 if (isset($_SESSION['antispam'][$m][$w])) {
   header("Content-Type: image/jpeg");
   readfile ($_SESSION['antispam'][$m][$w].'.jpg');
-  unset($_SESSION['antispam'][$m][$w]);
+  /* unset($_SESSION['antispam'][$m][$w]); */
 }
 ?>
