52dbc44079c8e97f88be39fe581bd3624bb25525
m0nu <m0nu@5b3287ef-0713-0410-bd1a-a55732392574>
2007-08-10 21:00:27 +0000

    neuer wysiwyg editor teil 1

--- include/admin/selfbp.php
+++ include/admin/selfbp.php
@@ -123,12 +123,11 @@ function edit_text ($t, $add) {
   if (!$erg AND $add) {
     $t = trim($t);
     $t = '<?php defined (\'main\') or die (\'no direct access\'); ?>'. $t;
-    $t = preg_replace("/\/([^>]*)>/","/\\1>\n",$t);
+    #$t = preg_replace("/\/([^>]*)>/","/\\1>\n",$t);
   } elseif ($erg AND !$add) {
     $t = preg_replace("/^\s*<\?php defined \('main'\) or die \('no direct access'\); \?>(.*)$/s","\\1",$t);
-    $t = preg_replace ("/<!--@(.*)@-->/", "", $t);
-    $t = preg_replace ("/(\015\012|\015|\012)/", "", $t);
-    
+    #$t = preg_replace ("/<!--@(.*)@-->/", "", $t);
+    #$t = preg_replace ("/(\015\012|\015|\012)/", "", $t);
   }
   return ($t);
 }
@@ -143,6 +142,47 @@ function save_file_to ($filename, $data, $flags = 0, $f = FALSE) {
   fclose($f);
 }
 
+function gallery_admin_showcats ( $id , $stufe ) {
+  $q = "SELECT * FROM prefix_gallery_cats WHERE cat = ".$id." ORDER BY pos";
+	$erg = db_query($q);
+	if ( db_num_rows($erg) > 0 ) {
+ 	  while ($row = db_fetch_object($erg) ) {
+	    echo '<tr class="Cmite"><td>'.$stufe.'- <a href="admin.php?selfbp-imagebrowser-'.$row->id.'">'.$row->name.'</a></td></tr>';
+		  gallery_admin_showcats($row->id, $stufe.' &nbsp;' );
+	  }
+	}
+}
+
+# gallery image browser
+if  ($menu->get(1) == 'imagebrowser') {
+  $cat = 0;
+  if ($menu->get(2) != '') {
+    $cat = escape($menu->get(2), 'integer');
+  }
+
+  $abf = "SELECT id,besch,datei_name,endung FROM prefix_gallery_imgs WHERE cat = ".$cat;
+  $erg = db_query($abf);
+  $i = 0;
+	$design = new design ( 'Admins Area', 'Admins Area', 0 );
+	$design->header();
+  $tpl = new tpl('selfbp-imagebrowser', 1);
+  $tpl->out(0);
+  gallery_admin_showcats(0,'');
+  $tpl->out(1);
+  while ($row = db_fetch_assoc($erg) ) {
+    if ( $i <> 0 AND ($i % $allgAr['gallery_imgs_per_line'] ) == 0 ) {
+      echo '</tr><tr>';
+    }
+    $toput = 'include/images/gallery/img_'.$row['id'].'.'.$row['endung'];
+    $pfad = 'include/images/gallery/img_thumb_'.$row['id'].'.'.$row['endung'];
+    $tpl->set('toput', $toput);
+    $tpl->set('pfad', $pfad);
+    $tpl->out(2);
+    $i++;
+  }
+  $design->footer(1);
+}
+
 $f = false;
 if ( !is_writable('./include/contents/selfbp/selfp') ) {
   $f = true;
@@ -155,13 +195,13 @@ if ( !is_writable('./include/contents/selfbp/selfb') ) {
   }
 }
 
-
-if ( isset ($_POST['text']) AND isset($_POST['filename']) AND isset($_POST['akl']) ) {
+if ( isset ($_POST['bbwy']) AND isset($_POST['filename']) AND isset($_POST['akl']) ) {
   # speichern
   $akl = $_POST['akl'];
-  $text = rteSafe($_POST['text']);
-  $text = set_properties(array('title'=>$_POST['title'],'hmenu'=>$_POST['hmenu'],'view'=>$_POST['view'],'viewoptions'=>$_POST['viewoptions'])).$text;
-  $text = edit_text($text, true);
+  $text = $_POST['bbwy'];
+  #$text = rteSafe($_POST['text']);
+  #$text = set_properties(array('title'=>$_POST['title'],'hmenu'=>$_POST['hmenu'],'view'=>$_POST['view'],'viewoptions'=>$_POST['viewoptions'])).$text;
+  $text = edit_text(stripslashes($text), true);
   
   $a = substr ( $akl, 0, 1);
   #$e = substr ( $akl, 1 );
@@ -170,37 +210,47 @@ if ( isset ($_POST['text']) AND isset($_POST['filename']) AND isset($_POST['akl'
   #  unlink ( 'include/contents/selfbp/self'.$a.'/'.$e );
   #}
   
+  if (!empty($_POST['exfilename']) AND $_POST['exfilename'] != $_POST['filename']) {
+    $exfilename = escape($_POST['exfilename'], 'string');
+    unlink('include/contents/selfbp/self'.$a.'/'.$exfilename);
+
+  } 
+  
   $filename = get_nametosave($_POST['filename']);
   $fname = 'include/contents/selfbp/self'.$a.'/'.$filename;
   save_file_to ( $fname, $text );
   
   wd ('admin.php?selfbp=0&akl='.$a.$filename, 'Ihre Aenderungen wurden gespeichert...', 3);
-} else {
-  #anzeigen
-  $tpl = new tpl ( 'selfbp', 1 );
-  $akl  = '';
-  if ( isset ( $_REQUEST['akl'] ) ) { $akl = $_REQUEST['akl']; }
-  
-  #löschen
-  if (isset($_REQUEST['del'] )){
-    $del=$_REQUEST['del'];
-    $a = substr ( $del, 0, 1);
-    $e = substr ( $del, 1 );
-  
-    if ( $e != 'neu' ) {
-      unlink ( 'include/contents/selfbp/self'.$a.'/'.$e );
-    }
-  }
-  
-  $text= get_text($akl);
-  $properties=get_properties($text);
-  $text = edit_text ($text, false);
-  $text = rteSafe($text);
-  $filename = get_filename($akl);
-  $akl  = get_akl ( $akl );
-  $view=get_view($properties['view']);
-  $tpl->set_ar_out(array('akl'=>$akl,'text'=>$text,'filename'=>$filename,'title'=>$properties['title'],'hmenu'=>$properties['hmenu'],'view'=>$view,'viewoptions'=>$properties['viewoptions']),0);
+  $design->footer(1);
 }
+
+#anzeigen
+$tpl = new tpl ( 'selfbp', 1 );
+$akl  = '';
+if ( isset ( $_REQUEST['akl'] ) ) {
+  $akl = $_REQUEST['akl'];
+}
+  
+#löschen
+if (isset($_REQUEST['del'] )){
+  $del=$_REQUEST['del'];
+  $a = substr ( $del, 0, 1);
+  $e = substr ( $del, 1 );
+ 
+  if ( $e != 'neu' ) {
+    unlink ( 'include/contents/selfbp/self'.$a.'/'.$e );
+  }
+}
+  
+$text= get_text($akl);
+#$properties=get_properties($text);
+$text = edit_text ($text, false);
+#$text = rteSafe($text);
+$filename = get_filename($akl);
+$akl  = get_akl ( $akl );
+#$view=get_view($properties['view']);
+$tpl->set_ar_out(array('akl'=>$akl,'text'=>$text,'filename'=>$filename,'exfilename'=>$filename),0);
+
 
 $design->footer();
 ?>
