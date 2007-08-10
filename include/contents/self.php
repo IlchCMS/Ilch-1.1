431b666683a8c976b78aa183316caadc5b045a34
m0nu <m0nu@5b3287ef-0713-0410-bd1a-a55732392574>
2007-08-10 21:08:02 +0000

    changelog nachgepflegt und self seite angepasst die alten properties raus gepackt

--- include/contents/self.php
+++ include/contents/self.php
@@ -5,26 +5,7 @@
 
 defined ('main') or die ( 'no direct access' );
 
-
-# liest die <!--@..=..@--> in den ersten 1024 Zeichen in ein Array aus
-function get_properties($file){
-      $handle = fopen ($file, 'r');
-      $buffer = fread($handle, 1024);
-      fclose ($handle);
-      preg_match_all("|(?:<!--@(?P<name>[^=]*)=(?P<value>.*)@-->)|U",$buffer, $out, PREG_SET_ORDER);
-      unset($buffer);
-      
-      $properties= array();
-      foreach($out as $x){
-        $properties[$x[name]]=$x[value];
-      }
-      unset($out);
-      return $properties;
-
-}
-
 if ( $menu->get(1) != '' ) {
-
   # moegliche endungen
 	$ende_ar = array ('.html','.htm','.php');
 	$um = $menu->get(1);
@@ -35,44 +16,16 @@ if ( $menu->get(1) != '' ) {
 	foreach ($ende_ar as $ext ) {
 	  $file='include/contents/selfbp/selfp/'.$menu->get(1).$ext;
     if ( file_exists ( $file ) ) {
-		  
-      $properties = get_properties($file);
-      
-      
-      if($properties['view'] == "fullscreen"){
-        require_once($file);
-      
-      }elseif($properties['view'] == "popup"){
-        if($menu->get(2)!='true'){
-          $title = $allgAr['title'].' :: '.$properties['title'];
-          $hmenu = $properties['hmenu'];
-          $design = new design ( $title , $hmenu );
-          $design->header();
-  
-?>
-          <script language="JavaScript" type="text/javascript">
-          <!--
-          var fenster = window.open('index.php?self-<?php echo $menu->get(1); ?>-true','Seite','<?php echo $properties['viewoptions']; ?>');
-          fenster.focus();
-           -->
-          </script>
-<?php
-          echo '<a href="index.php?self-'.$menu->get(1).'-true">'.$properties['title'].'</a>';
-        
-          $design->footer();
-        }else{
-          require_once($file);
-        }
-      }else{
-        $title = $allgAr['title'].' :: '.$properties['title'];
-        $hmenu = $properties['hmenu'];
+		
+        $title = $allgAr['title'].' :: '.ucfirst($menu->get(1));
+        $hmenu = ucfirst($menu->get(1));
         $design = new design ( $title , $hmenu );
         $design->header();
 
         require_once($file);
       
         $design->footer();
-      }
+
       $ok=true;
 			break;
     }
