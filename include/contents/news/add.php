2042a57d68ab07c042961e4a4a0ba28e553bcdd0
Marcel Moritz <info@maretz.eu>
2014-12-18 21:56:35 +0100

    viewtrip Chrome behoben, Anpassung weitere Dateien

--- include/contents/news/add.php
+++ include/contents/news/add.php
@@ -81,7 +81,7 @@ if (loggedin()) {
 		$tpl->set_out('SMILIES',getsmilies(),5);
     }
 } else {
-    echo 'Nur f&uuml;r angemeldete Benutzer';
+    echo '<div class="text-center"><span class="ilch_hinweis_rot">Nur f&uuml;r angemeldete Benutzer</span></div>';
 }
 $design->footer();
 ?>
