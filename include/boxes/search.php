15d587794f8289655841361aa8b81483442105a9
Marcel Moritz <info@maretz.eu>
2015-03-18 20:56:49 +0100

    search box chrome korrektur

--- include/boxes/search.php
+++ include/boxes/search.php
@@ -8,7 +8,7 @@ defined ('main') or die ( 'no direct access' );
 $suchtpl = <<<HTML
 <div class="text-center">
 <form action="index.php?search" method="GET">
-<input type="text" value="{search}" name="search" size="{size}" placeholder="Ich suche ....">
+<input type="text" value="{search}" name="search" size="15" placeholder="Ich suche ....">
 <input type="hidden" name="in" value="2">
 <input type="submit" value="{_lang_search}">
 </form>
